c2f4b355bcb638f0f0392a82262a155e2d878bbb
Maria Balsamo <mia.balsamo@gmail.com>
2017-12-04 23:58:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 function deleteFromObjectByKey(object, key) {
   delete object[key];
-  return object;
+  return obj;
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
